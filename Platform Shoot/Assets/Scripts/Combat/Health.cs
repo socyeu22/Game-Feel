9eb16b47c725f255bccee5e26b38d7842ab35f78
socyeu22 <lovesoc10000@gmail.com>
2024-10-22 21:53:13 +0700

    Add Splater Prefab and  Tile Sprite Mask Prefab

--- Platform Shoot/Assets/Scripts/Combat/Health.cs
+++ Platform Shoot/Assets/Scripts/Combat/Health.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private GameObject _splaterPrefab;
     [SerializeField] private int _startingHealth = 3;
 
     private int _currentHealth;
@@ -21,6 +22,14 @@ public class Health : MonoBehaviour
 
         if (_currentHealth <= 0) {
             Destroy(gameObject);
+            SpawDeathSplaterPrefab();
         }
+    }
+    private void SpawDeathSplaterPrefab() {
+        GameObject newSplaterPrefab = Instantiate(_splaterPrefab, transform.position, transform.rotation);
+        SpriteRenderer deathSplaterSpriteRenderer = newSplaterPrefab.GetComponent<SpriteRenderer>();
+        ColorChanger colorChanger = GetComponent<ColorChanger>();
+        Color curentColor = colorChanger.DefaultColor;
+        deathSplaterSpriteRenderer.color = curentColor;
     }
 }
